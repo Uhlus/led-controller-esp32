9e73c4a1ef711cba96d809f470bb32adb78027dd
tobias.uhl <tobias.uhl@aon.at>
2025-08-23 22:43:42 +0200

    Dev - Animation Test

--- main.ino
+++ main.ino
@@ -23,7 +23,7 @@
 
 /* -------------------------------- LED Sting ------------------------------- */
 #define LED_STRIP_PIN 2
-#define NUM_PIXELS 89
+#define NUM_PIXELS 534
 // #define NUM_PIXELS 300
 
 Animation_base NeoPixel;
